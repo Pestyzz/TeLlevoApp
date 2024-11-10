c2a71615d1b8642c20abb677aea12da04a1be9f1
Bastián Maximiliano Ñiripil Concha <ba.niripil@duocuc.cl>
2024-11-10 01:10:25 -0300

    added: trip database implementation + screen information

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,10 @@
-import { OnInit, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import { OnInit, Component, ElementRef, ViewChild, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Database, ref, set } from '@angular/fire/database';
 import { Geolocation } from '@capacitor/geolocation';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { TripInterface } from 'src/app/interfaces/trip.interface';
 
 declare var google: any;
 
@@ -15,17 +19,22 @@ export class MapComponent implements OnInit {
   @ViewChild('map') mapElementRef!: ElementRef;
   map: any;
   mapListener: any;
-  currentMarker: any;
   originMarker: any;
   destinationMarker: any;
   directionsService: any;
   directionsRenderer: any;
-  center: { lat: number, lng: number } = { lat: 0, lng: 0 };
+  center = { lat: 0, lng: 0 };
+
   activeProfile: 'passenger' | 'driver' | null;
 
-  constructor(private authService: AuthService) {
+  tripInfo: TripInterface | null = null;
+  tripPublished = false;
+  tripStarted = false;
+
+  constructor(private authService: AuthService, private database: Database, private router: Router, 
+    private alertController: AlertController, private cdr: ChangeDetectorRef) {
     this.activeProfile = this.authService.getActiveProfile();
-   }
+  }
 
   ngOnInit() {
     this.loadMap();
@@ -87,6 +96,7 @@ export class MapComponent implements OnInit {
         lng: place.geometry.location.lng()
       };
 
+      this.calculateRoute(destination);
       this.calculateAndDisplayRoute(this.center, destination);
     });
   }
@@ -94,6 +104,106 @@ export class MapComponent implements OnInit {
   setPassengerMode() {
     // Lógica específica para el Pasajero
     // Aquí se puede implementar la lógica para mostrar la lista de viajes
+  }
+
+  calculateRoute(destination: any) {
+    const origin = this.map.getCenter();
+    this.directionsService.route(
+      {
+        origin: origin,
+        destination: destination,
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (response: any, status: any) => {
+        if (status === 'OK') {
+          this.directionsRenderer.setDirections(response);
+          const route = response.routes[0].legs[0];
+          this.tripInfo = {
+            driver: this.authService.currentUserSig(),
+            vehicle: this.authService.vehicleSig(),
+            origin: {
+              name: route.start_address,
+              coords: origin.toJSON()
+            },
+            destination: {
+              name: route.end_address,
+              coords: destination
+            },
+            distance: route.distance.text,
+            duration: route.duration.text
+          };
+          this.cdr.detectChanges();
+        } else {
+          console.error('Directions request failed due to ' + status);
+        }
+      }
+    );
+  }
+
+  async publishTrip() {
+    const alert = await this.alertController.create({
+      header: 'Confirmar',
+      message: '¿Estás seguro de que deseas publicar este viaje?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'Publicar viaje',
+          handler: async () => {
+            if (!this.tripInfo) {
+              console.error('Trip information is missing');
+              return;
+            }
+
+            const tripRef = ref(this.database, `trip/${this.authService.firebaseAuth.currentUser?.uid}`);
+            localStorage.setItem('currentTrip', JSON.stringify(this.tripInfo));
+            await set(tripRef, this.tripInfo);
+            this.tripPublished = true;
+          },
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  startTrip() {
+    this.router.navigate(['/main/map']);
+    this.tripStarted = true;
+  }
+
+  cancelTrip() {
+    this.tripInfo = null;
+    this.tripPublished = false;
+    this.directionsRenderer.set('directions', null);
+    localStorage.removeItem('currentTrip');
+    this.cdr.detectChanges();
+  }
+  
+  calculateAndDisplayRoute(origin: { lat: number, lng: number }, destination: { lat: number, lng: number }) {
+    this.directionsService.route({
+      origin,
+      destination,
+      travelMode: google.maps.TravelMode.DRIVING
+    }, (response: any, status: any) => {
+      if (status === 'OK') {
+        this.directionsRenderer.setDirections(response);
+
+        if (this.originMarker) {
+          this.originMarker.setMap(null);
+        }
+        if (this.destinationMarker) {
+          this.destinationMarker.setMap(null);
+        }
+
+        this.originMarker = this.addCustomMarker(origin, 'Origin', 'assets/map/OriginIconPin.png');
+        this.destinationMarker = this.addCustomMarker(destination, 'Destination', 'assets/map/DestinationIconPin.png');
+      } else {
+        console.error('Directions request failed due to ' + status);
+      }
+    });
   }
 
   addCustomMarker(position: { lat: number, lng: number }, title: string, iconUrl: string) {
@@ -111,30 +221,6 @@ export class MapComponent implements OnInit {
       map: this.map,
       title,
       content: markerContent
-    });
-  }
-
-  calculateAndDisplayRoute(origin: { lat: number, lng: number }, destination: { lat: number, lng: number }) {
-    this.directionsService.route({
-      origin,
-      destination,
-      travelMode: google.maps.TravelMode.DRIVING
-    }, (response: any, status: any) => {
-      if (status === 'OK') {
-        this.directionsRenderer.setDirections(response);
-
-        if (this.originMarker) {
-          this.originMarker.map = null;
-        }
-        if (this.destinationMarker) {
-          this.destinationMarker.map = null;
-        }
-
-        this.originMarker = this.addCustomMarker(origin, 'Origin', 'assets/map/OriginIconPin.png');
-        this.destinationMarker = this.addCustomMarker(destination, 'Destination', 'assets/map/DestinationIconPin.png');
-      } else {
-        console.error('Directions request failed due to ' + status);
-      }
     });
   }
 }
---
+++ src/app/interfaces/trip.interface.ts
@@ -0,0 +1,20 @@
+export interface TripInterface {
+    driver: any;
+    vehicle: any;
+    origin: {
+        name: string,
+        coords: {
+            lat: number;
+            lng: number;
+        };
+    };
+    destination: {
+        name: string,
+        coords: {
+            lat: number;
+            lng: number;
+        };
+    };
+    distance: string;
+    duration: string;
+}
